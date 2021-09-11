6833831b1a83db297ffa292958ccf9a78582a5ea
AMAN khan <welcomebackaman@gmail.com>
2021-09-11 04:38:41 -0700

    new version only for zoom

--- bot/zoom.py
+++ bot/zoom.py
@@ -3,6 +3,7 @@ from config import Config
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from bot import updater, browser
 from telegram.ext import run_async
 from telegram import ChatAction
@@ -30,38 +31,37 @@ def joinZoom(context, url_meet, passStr):
     try:
         if os.path.exists("zoom.pkl"):
             cookies = pickle.load(open("zoom.pkl", "rb"))
-            browser.get('https://accounts.google.com/o/oauth2/auth/identifier?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3Abbc%2C16%3Afad07e7074c3d678%2C10%3A1601127482%2C16%3A9619c3b16b4c5287%2Ca234368b2cab7ca310430ff80f5dd20b5a6a99a5b85681ce91ca34820cea05c6%22%2C%22cdl%22%3Anull%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22d18871cbc2a3450c8c4114690c129bde%22%7D&response_type=code&flowName=GeneralOAuthFlow')
-            for cookie in cookies:
-                browser.add_cookie(cookie)
-        else:
-            context.bot.send_message(chat_id=userId, text="You're not logged in! Send /zlogin to login to zoom")
-            return
+            browser.get('https://zoom.us')
+            
+        
 
         browser.get('https://zoom.us/wc/join/'+ url_meet)
-        time.sleep(5)
-        browser.save_screenshot("ss.png")
-        context.bot.send_chat_action(chat_id=userId, action=ChatAction.UPLOAD_PHOTO)
-        mid  = context.bot.send_photo(chat_id=userId, photo=open('ss.png', 'rb'), timeout = 120).message_id
-        os.remove('ss.png')
-
-        browser.find_element_by_xpath('//*[@id="joinBtn"]').click()
-        time.sleep(5)
-        try: 
+        district = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#inputname")))
+        browser.find_element(By.CSS_SELECTOR, "#inputname").click()
+        for i in range(0, 20):
+            browser.find_element(By.CSS_SELECTOR, "#inputname").send_keys(Keys.BACK_SPACE)
+        browser.find_element(By.CSS_SELECTOR, "#inputname").send_keys("AMAN KHAN")
+        time.sleep(3)
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#joinBtn"))).click()
+        print("Clicked on join button")
+        try:
+             for button in WebDriverWait(browser, 30).until(EC.visibility_of_all_elements_located((By.XPATH, "//button[contains(., 'Continue')]"))):
+                 button.click()
+                 print("Trying to click the continue button")
+        except Exception as e:
+            print(e)
+            print("Is it a good error, or a bad error ? Sore wa.... yami no nakae")
+        try:
             WebDriverWait(browser, 2400).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="inputpasscode"]'))).send_keys(passStr)
+            print("Entered the code")
         except:
             context.bot.send_message("Meeting didn't start. Probably Cancelled")
 
-        browser.find_element_by_xpath('//*[@id="joinBtn"]').click()
-
-        time.sleep(10)
-
-        context.bot.delete_message(chat_id=userId ,message_id = mid)
-
-        browser.save_screenshot("ss.png")
-        context.bot.send_chat_action(chat_id=userId, action=ChatAction.UPLOAD_PHOTO)
-        mid = context.bot.send_photo(chat_id=userId, photo=open('ss.png', 'rb'), timeout = 120).message_id
-        os.remove('ss.png')
-
+        WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#joinBtn"))).click()
+        print("Clicked join after entering passcode")
+        time.sleep(15)
+       
+        
         WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="voip-tab"]/div/button'))).click()
 
         WebDriverWait(browser, 1000).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="wc-footer"]/div/div[2]/button[1]'))).click()
--- bot/zoom_schedule.py
+++ bot/zoom_schedule.py
@@ -4,8 +4,8 @@ from bot import updater, browser
 from telegram.ext import run_async
 from telegram import ChatAction
 import os
-import pickle
 import time
+import pickle
 from os import execl
 from sys import executable
 import csv
@@ -17,12 +17,9 @@ meeting_list = list()
 def convertTime(string_time):
     hour = string_time.split(':')[0]
     minute = string_time.split(':')[1]
-
     datetime_str = f'{datetime.date.today().strftime("%m/%d/%y")} {hour}:{minute}:00'
     datetime_object = datetime.datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
-
     return datetime_object
-
 
 class Meeting():
     def __init__(self, name, day, time, mid, mpass):
@@ -40,24 +37,34 @@ def getTodayMeetings():
                     meeting = Meeting(row[0], row[1], row[2], row[3], row[4])
                     meeting_list.append(meeting)
     except:
-        print("Zoom schedule not found. Run schedule.py to add schedule or change SCHEDULE environment variable to 'False' to turn off schedule")
+        print("Zoom schedule not found. Disabling Scheduler")
+        Config.SCHEDULE = 'False'
 
 def zoom(context):
     logging.info("DOING") 
     userId = Config.USERID
-    context.bot.send_chat_action(chat_id=userId, action=ChatAction.TYPING) 
-
     url_meet = context.job.context[0]
     passStr = context.job.context[1]
-    context.bot.send_message(chat_id=userId, text="Attending " + str(context.job.context[2]))
-    joinZoom(context, url_meet, passStr)
+    context.bot.send_message(chat_id=userId, text="TIME FOR MEETING: " + str(context.job.context[2]))
+    joinZoom(context, url_meet, passStr, userId)
+
+def timeTable(update, context):
+    if Config.SCHEDULE == 'False':
+        context.bot.send_message(chat_id=update.message.chat_id, text="Scheduler is Disabled")
+    if Config.SCHEDULE == 'True':
+        text = "Today's Meeting: \n \n"
+        for row in meeting_list:
+            text+=str(row.name) + " at " + str(row.time).split()[1] + "\n"
+        context.bot.send_message(chat_id=update.message.chat_id, text=text)
+        try:
+            context.bot.send_document(chat_id=update.message.chat_id, document=open('bot/zoom.csv', 'rb'), caption="Entire Schedule", timeout = 120)
+        except:
+            pass
 
 def zJobQueue():
-    logging.info("Adding Zoom meetings to schedule")
+    logging.info("Adding Timetable Data")
     getTodayMeetings()
-
-    j = updater.job_queue
-    
+    j = updater.job_queue    
     for row in meeting_list:
         secs = (row.time - datetime.datetime.now()).total_seconds()
         if(secs > 0):
